c7b5ead7d780495b3160e9ed0b9d64a7aaced4f0
Tim Licata <tim@timothylicata.com>
2015-02-02 22:19:26 -0800

    working example with ball and edge detection

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 var Physics = require("physicsjs");
 
-console.log("hello, Physics", Physics);
+Physics(function (world) {
+
+  var viewWidth = 500;
+  var viewHeight = 300;
+
+  var renderer = Physics.renderer("canvas", {
+    el: "game",
+    width: viewWidth,
+    height: viewHeight,
+    meta: false
+  });
+  world.add(renderer);
+
+  var ball = Physics.body("circle", {
+    x: 50,
+    y: 30,
+    vx: 0.2,
+    vy: 0.01,
+    radius: 20
+  });
+  world.add(ball);
+
+  var viewportBounds = Physics.aabb(0, 0, viewWidth, viewHeight);
+  world.add(Physics.behavior("edge-collision-detection", {
+    aabb: viewportBounds,
+    restitution: 0.99,
+    cof: 0.99
+  }));
+  world.add(Physics.behavior("body-impulse-response"));
+
+  world.on("step", function(){
+    world.render();
+  });
+
+  Physics.util.ticker.on(function (time) {
+    world.step(time);
+  });
+  Physics.util.ticker.start();
+});
